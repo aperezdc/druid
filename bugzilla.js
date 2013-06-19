bfd5f2178956ee91dae6319036bb83a251342e97
Adrian Perez de Castro <aperez@igalia.com>
2013-06-19 22:34:40 +0300

    Bugzilla: Allow <prefix><space><bugid> patterns

--- bugzilla.js
+++ bugzilla.js
@@ -14,7 +14,7 @@ var ExpandBugId = Trigger.$extend({
 	__init__: function (prefix, baseurl) {
 		var self = this;
 		self.baseurl = baseurl;
-		self.$super(new RegExp("\\b" + prefix + "[:#](\\d+)\\b", "i"), function (req, match) {
+		self.$super(new RegExp("\\b" + prefix + "[:# ](\\d+)\\b", "i"), function (req, match) {
 			req.reply(self.baseurl + "/show_bug.cgi?id=" + match[0]);
 			return true;
 		}, true);
